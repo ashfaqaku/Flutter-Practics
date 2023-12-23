4e4cb534083d826aaab7b311ce12c00bd9a8d1b1
Muhammad Ashfaq <ashfaqaku@gmail.com>
2023-12-23 19:02:50 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,4 @@
+/*Q1*/
 void main() {
   List<int> numbers = [15, 28, 3, 41, 7, 52, 19, 64, 2, 30];
 int smallest = numbers[0];
@@ -13,3 +14,25 @@ for (var i =1; i<numbers.length; i ++){
     print('${smallest}');
     print('${gratest}');
 }
+/*Q2*/
+void main() {
+  List<String> usersEligibility = ['John', 'Alice', 'eligible', 'Mike', 'Sarah', 'Tom'];
+
+  usersEligibility.removeWhere((e) => e == 'eligible');
+
+  print(usersEligibility);  
+}
+/*Q3*/
+void main() {
+  List<int> numbers = [15, 28, 3, 41, 7, 52, 19, 64, 2, 30];
+    int gratest = numbers[0];
+    for (var i =1; i<numbers.length; i ++){
+        if (numbers[i]>gratest){
+            gratest = numbers[i];
+        }
+    }
+    print('${gratest}');
+}
+
+/*Q4*/
+
