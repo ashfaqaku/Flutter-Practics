a14c3b53bf59ea057e2d661ed17c0952be2fb249
Muhammad Ashfaq <ashfaqaku@gmail.com>
2023-12-23 20:20:45 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -36,3 +36,52 @@ void main() {
 
 /*Q4*/
 
+void main() {
+  List<String> originalList = ['apple', 'banana', 'apple', 'orange', 'banana', 'kiwi'];
+  List<String> uniqueList = originalList.toSet().toList();
+
+  print(uniqueList); 
+}
+/*Q5*/
+void main() {
+    List<int> numbers = [15, 28, 3, 41, 4,6,7, 52, 19, 64, 2, 30];
+    for (int num in numbers) {
+        if (num %2==0) {
+            print('${num}');
+        }
+    }
+}
+/*Q6*/
+
+import 'dart:io';  
+
+void main() {
+  print('Enter a list of numbers separated by commas:');
+  
+  String? input = stdin.readLineSync();
+    
+  List<String> numberStrings = input!.split(',');
+    
+  List<int> evenNumbers = numberStrings.map((e) => int.tryParse(e.trim()) ?? 0) 
+                                       .where((num) => num % 2 == 0)          
+                                       .toList();                             
+  
+  print('Even numbers from the list: $evenNumbers');
+}
+/*Q7*/
+
+void main() {
+  Map<String, double> mathMarks = {
+    'ram': 30,
+    'mark': 32,
+    'harry': 88,
+    'raj': 69,
+    'john': 15,
+  };
+
+  mathMarks.removeWhere((key, value) => value <= 30);
+
+  print(mathMarks);
+}
+
+
